77997c6867b6d2fa3e479e735cd86d1dfbbfe98f
Ikraam <IbnDaanis@Gmail.com>
2021-02-17 15:38:45 +0000

    Changed data structure and added methods

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,21 @@
 class TodoList {
   #list
   constructor() {
-    this.#list = []
+    this.#list = {}
   }
 
   getList = () => this.#list
+
   addTodo = todo => {
-    this.list.push(todo)
+    this.#list[todo.id] = todo
     return this
   }
   removeTodo = todo => {
+    delete this.#list[todo.id]
     return this
   }
-  editTodo = todo => {
+  editTodo = (todo, edited) => {
+    this.#list[todo.id] = { ...this.#list[todo.id], ...edited }
     return this
   }
 }
@@ -22,7 +25,19 @@ class Todo {
     this.title = title
     this.isCompleted = isCompleted
     this.isDeleted = false
+    this.id = Math.random().toString(36).substr(2, 9)
   }
 }
 
 const newTodo = new TodoList()
+const first = new Todo('First', false)
+const second = new Todo('Second', false)
+
+console.log(
+  newTodo
+    .addTodo(first)
+    .addTodo(second)
+    .removeTodo(first)
+    .editTodo(second, { isCompleted: true })
+    .getList()
+)
