668cd58366cd2cd96a34b9af24458b4a2df6d208
Ikraam <IbnDaanis@Gmail.com>
2021-02-17 17:25:55 +0000

    Minor changes

--- scripts/app.js
+++ scripts/app.js
@@ -4,14 +4,16 @@ class TodoList {
     this.#list = {}
   }
 
-  getList = () => this.#list
+  getList = () => {
+    return this.#list
+  }
 
   addTodo = todo => {
     this.#list[todo.id] = todo
     return this
   }
-  removeTodo = todo => {
-    delete this.#list[todo.id]
+  removeTodo = id => {
+    delete this.#list[id]
     return this
   }
   editTodo = (todo, edited) => {
@@ -24,20 +26,14 @@ class Todo {
   constructor(title, isCompleted) {
     this.title = title
     this.isCompleted = isCompleted
-    this.isDeleted = false
     this.id = Math.random().toString(36).substr(2, 9)
   }
 }
 
-const newTodo = new TodoList()
-const first = new Todo('First', false)
-const second = new Todo('Second', false)
+const newTodoList = new TodoList()
 
-console.log(
-  newTodo
-    .addTodo(first)
-    .addTodo(second)
-    .removeTodo(first)
-    .editTodo(second, { isCompleted: true })
-    .getList()
-)
+for (let i = 0; i < 10; i++) {
+  newTodoList.addTodo(new Todo(i, false))
+}
+
+console.log(newTodoList.getList())
